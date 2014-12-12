b17794b8dd4696670a67b8fa5d48d22180a393cb
Tsvetelin-I <sog_ceci@abv.bg>
2014-12-12 21:37:16 +0200

    Update main.rb

--- main.rb
+++ main.rb
@@ -3,6 +3,15 @@ print output
 Dir.chdir("#{ARGV[1]}")
 puts Dir.pwd
 files = `ls`
-p files
 file = files.split("\n")
-puts file[]
+
+for i in file
+	ex = i.split(".")[0..1]
+	if ex[1]!= "c"
+		puts i + " : this file is not a C file"
+	else
+		puts "compiling " + i
+		print `gcc #{i}`
+		puts
+	end
+end 
