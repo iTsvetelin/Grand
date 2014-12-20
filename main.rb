a6f1edd186708e8386aad8d43fb4ed9653099145
Tsvetelin-I <sog_ceci@abv.bg>
2014-12-20 12:17:45 +0200

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,24 +1,33 @@
-output = `gcc #{ARGV[0]}`
-print output
-Dir.chdir("#{ARGV[1]}")
+Dir.chdir("#{ARGV[0]}")
 puts Dir.pwd
 files = `ls`
 file = files.split("\n")
 puts file
 puts "=================================="
+parse = Array.new()
 
 #looping through all files
 for i in file
+	print "editing file : " + i + " "
 	ex = i.split(".")[0..1]
 	if ex[1]!= "c"
-		puts i + " : this file is not a C file, or its name is wrong"
+		puts " : this file is not a C file, or its name is wrong"
 	else
-		puts "compiling " + i
-		c_result = `gcc #{i}`
-		if c_result = ""
-			puts "compilation succeed"
-		else
-			puts c_result
+		puts "compiling "
+		`gcc #{i}`
+
+		
+		File.open("#{i}").each do |line|
+			if line.to_s == "{"
+				puts "wrong syntax"
+			end
+			red_1 = line.gsub(/\t/,"").gsub(/ /,"").split("(")
+			parse << line.gsub(/\t/,"").gsub(/ /,"")
+			if red_1[0] == "if"
+				puts line
+			end
 		end
 	end
 end 
+
+puts parse
