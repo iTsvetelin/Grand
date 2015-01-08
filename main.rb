f3e652edc652c22f22acf2b87d1b4f9a463ff077
Tsvetelin-I <sog_ceci@abv.bg>
2015-01-09 00:52:27 +0200

    Update main.rb

--- main.rb
+++ main.rb
@@ -5,29 +5,40 @@ file = files.split("\n")
 puts file
 puts "=================================="
 parse = Array.new()
+$i=0
+inblock = false
 
 #looping through all files
 for i in file
 	print "editing file : " + i + " "
 	ex = i.split(".")[0..1]
 	if ex[1]!= "c"
-		puts " : this file is not a C file, or its name is wrong"
+		puts " - this is not a C file"
 	else
 		puts "compiling "
 		`gcc #{i}`
 
 		
 		File.open("#{i}").each do |line|
-			if line.to_s == "{"
-				puts "wrong syntax"
-			end
+			#if line.gsub(/\t/,"").gsub(/ /,"").gsub(/\n/,"")== "{"
+			#	puts "wrong syntax conventions"
+			#end
 			red_1 = line.gsub(/\t/,"").gsub(/ /,"").split("(")
-			parse << line.gsub(/\t/,"").gsub(/ /,"")
+			#parse << line.gsub(/\t/,"").gsub(/ /,"").gsub(/\n/,"")
 			if red_1[0] == "if"
+				inblock = true
 				puts line
+				line.each_char do |c|
+					if c.eql?("{")
+						$i+=1
+					elsif c.eql?("}")	 	 
+						$i-=1
+					end
+				end
 			end
+			puts $i
 		end
 	end
 end 
+#print parse
 
-puts parse
