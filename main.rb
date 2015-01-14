0c132f64716f9c5d8a2852a2dbcd08b35634b2e0
Tsvetelin-I <sog_ceci@abv.bg>
2015-01-14 10:26:31 +0200

    Update main.rb

--- main.rb
+++ main.rb
@@ -4,7 +4,9 @@ files = `ls`
 file = files.split("\n")
 puts file
 puts "=================================="
+sub = Array.new()
 parse = Array.new()
+$c=0
 $i=0
 inblock = false
 
@@ -14,26 +16,33 @@ for i in file
 	ex = i.split(".")[0..1]
 	if ex[1]!= "c"
 		puts " - this is not a C file"
+		next
 	else
 		puts "compiling "
 		`gcc #{i}`
 
 		
 		File.open("#{i}").each do |line|
-			#if line.gsub(/\t/,"").gsub(/ /,"").gsub(/\n/,"")== "{"
-			#	puts "wrong syntax conventions"
-			#end
-			red_1 = line.gsub(/\t/,"").gsub(/ /,"").split("(")
-			#parse << line.gsub(/\t/,"").gsub(/ /,"").gsub(/\n/,"")
-			if inblock == false
+			parse << line.gsub(/\t/,"").gsub(/ /,"").gsub(/\n/,"")
+		end
+		parse.each do |line|
+			red_1 = line.split("(")
 				if red_1[0] == "if"
-					inblock = true
-					puts line
+					$c+=1
+					if inblock == false
+						inblock = true
+					end
+					red_2 = line.split(")")
+					if red_2[1] == nil
+						$i+=1
+					end
 				end
-			end
 			if inblock
+				puts line
 				line.each_char do |c|
-					if c.eql?("{")
+					if c.eql?("{") && line.start_with?("{") == false
+						$i+=1
+					elsif line.start_with?("else") || line.start_with?("elseif")
 						$i+=1
 					elsif c.eql?("}")	 	 
 						$i-=1
@@ -41,12 +50,22 @@ for i in file
 				end
 				if $i==0
 					inblock = false
+					if $c > 2
+						sub << "too complicated if constructions"
+					end
 					puts "BLOCK ENDED"
+					$c=0
 				end
 			end
 			#puts $i
 		end
 	end
+	File.open("#{i}_results.txt","w") do |line|
+		line << sub
+	end
+	puts "File : " + i + "  ENDED EDITING  "
+	parse.clear
+	sub.clear
 end 
-#print parse
+#p parse
 
