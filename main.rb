c452991d9e8cfa89c671e3a2bf72a9eb406c0569
Tsvetelin-I <sog_ceci@abv.bg>
2015-01-19 10:20:53 +0200

    Update main.rb

--- main.rb
+++ main.rb
@@ -6,9 +6,13 @@ puts file
 puts "=================================="
 sub = Array.new()
 parse = Array.new()
+$lines=0
 $c=0
 $i=0
 inblock = false
+infunc = false
+type = false
+inboth = false
 
 #looping through all files
 for i in file
@@ -27,17 +31,33 @@ for i in file
 			parse << line.gsub(/\t/,"").gsub(/ /,"").gsub(/\n/,"")
 		end
 		parse.each do |line|
+			if line.start_with?("int") || line.start_with?("double") || line.start_with?("void") ||
+				 line.start_with?("char") || line.start_with?("float")
+				 type = true
+			end
 			red_1 = line.split("(")
-				if red_1[0] == "if"
-					$c+=1
-					if inblock == false
-						inblock = true
-					end
-					red_2 = line.split(")")
-					if red_2[1] == nil
-						$i+=1
-					end
+			if type == true
+				if red_1[0]!= "int" && red_1[0]!= "double" && red_1[0]!= "void" &&
+					red_1[0]!= "char" && red_1[0]!= "float"
+					infunc = true
+					type = false
+					puts line
 				end
+			end
+			if red_1[0] == "if"
+				$c+=1
+				if inblock == false
+					inblock = true
+				end
+				red_2 = line.split(")")
+				if red_2[1] == nil
+					$i+=1
+				end
+			end
+			if infunc
+				lines +=1
+			end
+
 			if inblock
 				puts line
 				line.each_char do |c|
@@ -50,7 +70,8 @@ for i in file
 				if $i==0
 					inblock = false
 					if $c > 2
-						sub << "too complicated if constructions"
+						sub << "too complicated if constructions "
+						sub << "super duper"
 					end
 					puts "BLOCK ENDED"
 					$c=0
@@ -65,7 +86,9 @@ for i in file
 	end
 	Dir.chdir("#{ARGV[0]}/results")
 	File.open("(#{i})results.txt","w") do |line|
-		line << sub
+		sub.each do |el|
+			line << el + "\n"
+		end
 	end
 
 	parse.clear
