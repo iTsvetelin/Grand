44693989fbd135f86e5ece2f82313785654c284f
Tsvetelin-I <sog_ceci@abv.bg>
2015-02-19 10:48:39 +0200

    Update main.rb

--- main.rb
+++ main.rb
@@ -4,21 +4,22 @@ files = `ls`
 file = files.split("\n")
 puts file
 puts "=================================="
-sub = Array.new()
+first_info = Array.new()
+info = Array.new()
 parse = Array.new()
 #complie = Array.new()
 $lines=0
 $liness=0
-$c=0
-$i=0
-$f=0
-$b_f=0
-$count=0
+$if_count=0
+$block_count=0
+$normal_func=0
+$big_func=0
+$lines_count=0
 inblock = false
 infunc = false
-inboth = false
 $r_brackets =0
 $w_brackets =0
+$lines_limit=10
 
 #looping through all files
 for i in file
@@ -38,14 +39,14 @@ for i in file
 		end
 
 		parse.each do |line|
-			$count += 1
+			$lines_count += 1
 			if line.length > 78
-				sub << "Lines should not exceed 78 characters @line" + $count.to_s
+				info << "Lines should not exceed 78 characters @line" + $lines_count.to_s
 			end
 			red_1 = line.split("(")
 			red_2 = line.split(")")
 			red_3 = line.split("[")
-			#p $i
+
 			if line == ("{")
 				$w_brackets+=1.0
 			end
@@ -60,22 +61,22 @@ for i in file
 					inblock = true
 					infunc = true
 					if red_2[1] == nil
-						$i+=1
+						$block_count+=1
 						$lines-=1
 					end
 				elsif red_1[0]!= "intmain" && red_1[1]== nil && red_3[1]== nil
 					if line.downcase! != nil
-						sub << "wrong variable naming @line" + $count.to_s
+						info << "wrong variable naming @line" + $lines_count.to_s
 					end
 				end
 			end
 			if red_1[0] == "if"
-				$c+=1
+				$if_count+=1
 				if inblock == false
 					inblock = true
 				end
 				if red_2[1] == nil
-					$i+=1
+					$block_count+=1
 				end
 			end
 
@@ -84,33 +85,33 @@ for i in file
 				if infunc
 					if line!="" && line.start_with?("//") == false
 						$lines+=1
-						if $c==1
+						if $if_count==1
 							$liness+=1
 						end
 					end
 				end
 				line.each_char do |c|
 					if c.eql?("{") && line.start_with?("{") == false
-						$i+=1
+						$block_count+=1
 					elsif c.eql?("}")	 	 
-						$i-=1
+						$block_count-=1
 					end
 				end
-				if $i==0
+				if $block_count==0
 					inblock = false
-					if $c > 2
-						sub << "too complicated if construction"
+					if $if_count > 2
+						info << "too complicated if construction"
 					end
 					#puts "BLOCK ENDED"
-					$c=0
+					$if_count=0
 					if infunc
 						if $lines > 5
-							$b_f+=1.0
+							$big_func+=1.0
 						else
-							$f+=1.0
+							$normal_func+=1.0
 						end
 						if $lines - $liness == 1
-							sub << "there is a function which can be replaced with trinary operator"
+							info << "there is a function which can be replaced with trinary operator"
 						end
 						$lines=0
 						$liness=0
@@ -118,7 +119,7 @@ for i in file
 					infunc=false
 				end
 			end
-
+			red_3.clear
 			red_2.clear
 			red_1.clear
 			#puts $i
@@ -126,23 +127,27 @@ for i in file
 	end
 
 
-	if $f==0 && $b_f==0
-		sub << "there are no functions"
-	elsif $f>0 && $b_f==0
-		sub <<  "all functions are  fine"
-	elsif $f==0 && $b_f>0
-		sub << "all functions are too big"
-	elsif $f/$b_f <=1.0
-		sub << "50% of the functions are too big"
+	if $normal_func==0 && $big_func==0
+		info << "there are no functions"
+	elsif $normal_func>0 && $big_func==0
+		info <<  "all functions are  fine"
+	elsif $normal_func==0 && $big_func>0
+		info << "all functions are too big"
+	elsif $normal_func/$big_func <=1.0
+		info << "50% of the functions are too big"
 	end
 	if $w_brackets ==0
-		sub << "all brackets are fine"
+		info << "all brackets are fine"
 	elsif $r_brackets/$w_brackets <=1.0
-		sub << "50% of the brackets are wrongly placed"
+		info << "50% of the brackets are wrongly placed"
 	end
+	first_info << "source code is total of " + $lines_count.to_s + " lines"
 
-	$f=0
-	$b_f=0
+	if $lines_count > $lines_limit
+		info << "the source code is too big"
+	end
+	$normal_func=0
+	$big_func=0
 	$w_brackets=0
 	$r_brackets=0
 
@@ -153,12 +158,13 @@ for i in file
 	end
 	Dir.chdir("#{ARGV[0]}/homew_results")
 	File.open("(#{i})result.txt","w") do |line|
-		sub.each do |el|
+		info.each do |el|
 			line << el + "\n"
 		end
 	end
-	$count=0
+	$lines_count=0
 	parse.clear
-	sub.clear
+	info.clear
+	first_info.clear
 end 
 
